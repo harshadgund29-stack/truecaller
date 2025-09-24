0099be6166052b4636101c505f506a61ce62aad2
harshadgund29-stack <harshadgund29@gmail.com>
2025-09-24 07:58:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ContactList from './ContactList';
+import ContactList from './Contactlist';
 import SearchBar from './SearchBar';
 import './App.css';
 
